e89f3d92ad95076219b9350bb5560c1307eb9880
Onelong <zzfeihua@163.com>
2018-06-16 20:44:17 +0800

    first commit

---
+++ Products/HDOpenUDID.framework/Headers/HDOpenUDID.h
@@ -0,0 +1,20 @@
+//
+//  HDOpenUDID.h
+//  HDOpenUDID
+//
+//  Created by 黄隆 on 2018/6/16.
+//  Copyright © 2018年 黄隆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDOpenUDID.
+FOUNDATION_EXPORT double HDOpenUDIDVersionNumber;
+
+//! Project version string for HDOpenUDID.
+FOUNDATION_EXPORT const unsigned char HDOpenUDIDVersionString[];
+
+#import <HDOpenUDID/OpenUDID.h>
+// In this header, you should import all the public headers of your framework using statements like #import <HDOpenUDID/PublicHeader.h>
+
+
