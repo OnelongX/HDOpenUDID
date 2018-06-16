40e4fdb4b5e69abe48fb95e20baf08d422bc2232
Onelong <zzfeihua@163.com>
2018-06-16 21:10:03 +0800

    0.0.2

--- HDOpenUDID-0.0.1/build/Pods.build/Release-iphonesimulator/HDOpenUDID.build/Objects-normal/x86_64/HDOpenUDID-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /var/folders/q8/rc2l853j45n_3l_vqw1hjcp80000gn/T/cocoapods-g4gxzvp2/Pods/Target\ Support\ Files/HDOpenUDID/HDOpenUDID-dummy.m \
-  /var/folders/q8/rc2l853j45n_3l_vqw1hjcp80000gn/T/cocoapods-g4gxzvp2/Pods/Target\ Support\ Files/HDOpenUDID/HDOpenUDID-prefix.pch
